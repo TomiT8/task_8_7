3265048df0e564e29461fef98ce9d0b16fc7d1ca
TomiT8 <tomas.jasko8@gmail.com>
2024-07-10 20:21:28 +0200

    pridanie generátora map a filter

--- 2024_07_08_tasks.py
+++ 2024_07_08_tasks.py
@@ -22,55 +22,72 @@
 
 # todo - task2 - csv
 
-from typing import List
-from dataclasses import dataclass
-import csv
-
-
-@dataclass
-class Movie:
-    MovieID: int
-    Title: str
-    Genre: str
-    Rating: float
-    Year: int
-
-
-def read_movies_csv(file_name):
-    movies = []
-    with open(file_name, mode='r') as filecsv:
-        reader = csv.reader(filecsv)
-        for row in reader:
-            movie = Movie(
-                MovieID=int(row[0]),
-                Title=row[1],
-                Genre=row[2],
-                Rating=float(row[3]),
-                Year=int(row[4])
-            )
-            movies += [movie]
-    return movies
-
+# from typing import List
+# from dataclasses import dataclass
+# import csv
+#
+# @dataclass
+# class Movie:
+#     MovieID: int
+#     Title: str
+#     Genre: str
+#     Rating: float
+#     Year: int
+#
+#
+# def read_movies_csv(file_name):
+#     movies = []
+#     with open(file_name, mode='r') as filecsv:
+#         reader = csv.reader(filecsv)
+#         for row in reader:
+#             movie = Movie(
+#                 MovieID=int(row[0]),
+#                 Title=row[1],
+#                 Genre=row[2],
+#                 Rating=float(row[3]),
+#                 Year=int(row[4])
+#             )
+#             movies += [movie]
+#     return movies
+#
+# # file_name = 'movies.csv'                                        # zapni ak chceš zobraziť celý zoznam csv a vypni nižšie filter
+# # movies = read_movies_csv(file_name)
+# # for movie in movies:
+# #     print(movie)
+#
+#
+# def filter_movies(movies: List[Movie], genre: str):
+#     filtered_movies = []
+#     for movie in movies:
+#         if movie.Genre.lower() == genre.lower():
+#             filtered_movies.append(movie)
+#     return filtered_movies
+#
 # file_name = 'movies.csv'
 # movies = read_movies_csv(file_name)
-# for movie in movies:
+#
+# # Filtruj filmy podľa žánru "Action"
+# filtered_movies = filter_movies(movies, 'Action')
+#
+# # Výpis:
+# for movie in filtered_movies:
 #     print(movie)
 
 
-def filter_movies(movies: List[Movie], genre: str):
-    filtered_movies = []
-    for movie in movies:
-        if movie.Genre == genre:
-            filtered_movies.append(movie)
-    return filtered_movies
+# todo - task3 - json
 
-file_name = 'movies.csv'
-movies = read_movies_csv(file_name)
+from dataclasses import dataclass
+@dataclass
+class Book:
+    title: str
+    author: str
+    year: int
+    isbn: str
 
-# Filtruj filmy podľa žánru "Action"
-filtered_movies = filter_movies(movies, 'Action')
+def __str__(self):
+    return f'{self.title}ôby {self.autor} from {self.year}. ISBN: {self.isbn}'
 
-# Výpis:
-for movie in filtered_movies:
-    print(movie)
-
+def print_books(books: List[Book]):
+    for book in books:
+        print(f"{book.title:<30} {book.author:<25} {book.year:<6} {book.isbn:<15}")
+    print(f"\nTotal number of books: {len(books)}")
---
+++ 2024_07_10.py
@@ -0,0 +1,71 @@
+# todo - Správce kontextu (správca kontextu)
+# https://journey.study/v2/learn/courses/10302/modules/26102/units/9/materials/44447
+
+# with
+
+# Vytvorenie vlastného správcu
+
+# tu bol task -> Vlastní správca kontextu jako funkce
+
+# todo - Generátory a iterátory
+## Iterace
+## Iterovanie cez veľkú kolekciu
+## Iterátor
+## Generátor
+
+
+# todo Task 1
+# Napis generator `even_numbers()`, ktory bude generovat donekonecna parne (sude) cisla
+# def even_numbers():
+#     x = 0
+#     while True:
+#         yield x
+#         x += 2
+#
+# generator = even_numbers()
+#
+# for i in range(10):
+#     print(next(generator))
+
+# todo Task 2
+# Napis generator `my_range(start, stop=None, step=1), ktory sa sprava rovnako ako funkcia range
+
+
+# todo Task 3
+# Napis generator `fib()`, ktory bude donekonecna generovat cisla fibonnaciho postupnosti
+
+# todo Task 4
+# Napis generator `read_file(file_name)`, ktory otvori subor `file_name` a bude
+# postupne vracat jeho riadky
+
+# todo Task 5
+# Napis generator `randgen(start, end)`, ktory bude donekonecna generovat nahodne cisla
+# v intervale <start, end>.
+
+# todo Task 6
+# Napis generatory `map` a `filter`, ktore sa spravaju ako builtin funkcie `map` a `filter`
+
+def my_map(f, iterable):
+    for item in iterable:
+        yield f(item)
+
+def to_lower(x):
+    return x.lower()
+
+strings = ['Mama', 'OTEC', 'seSTra', 'braT']
+lower_string = map(to_lower, strings)
+print(list(lower_string))
+
+
+
+def my_filter(f, iterable):
+    for item in iterable:
+        if f(item):
+            yield item
+
+def is_odd(x):
+    return x%2 != 0
+
+numbers = [1, 2, 3, 4, 5]
+odd_numbers = filter(is_odd, numbers)
+print(list(odd_numbers))
